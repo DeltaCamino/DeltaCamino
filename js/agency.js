6d6f8688c51bd8666d2ba9bcc25747fccda69ece
Damian Fijorek <damianfijorek@gmail.com>
2015-12-06 13:37:38 +0100

    Initial commit for Delta Camino site

--- js/agency.js
+++ js/agency.js
@@ -6,10 +6,10 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
+    $('a.page-scroll.btn').bind('click', function(event) {
+        var anchor = $(this).attr('href');
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $(anchor).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
